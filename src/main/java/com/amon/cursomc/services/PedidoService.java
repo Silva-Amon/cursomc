1b81b98f39cca7f24e63ee137f36d6f3fb70ebae
Amon Silva <Amon Silva@italtel.com.br>
2021-06-15 06:40:59 -0300

    PUT, DELETE E GET PARA CLIENTE

---
+++ src/main/java/com/amon/cursomc/services/PedidoService.java
@@ -0,0 +1,22 @@
+package com.amon.cursomc.services;
+
+import com.amon.cursomc.domain.Pedido;
+import com.amon.cursomc.repositories.PedidoRepository;
+import com.amon.cursomc.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PedidoService {
+
+    @Autowired
+    private PedidoRepository repo;
+
+    public Pedido buscar(Integer id){
+        Optional<Pedido> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+    }
+}
